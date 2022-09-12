0263a9b64c7349fbf0b1a4d1bc68dee8febd61aa
Lucas Calderon <lucas8calderon@hotmail.com>
2022-09-12 17:51:57 -0300

    tela de login, edittext

--- app/src/main/java/com/julioprojects/appplantinhas/LoginActivity.java
+++ app/src/main/java/com/julioprojects/appplantinhas/LoginActivity.java
@@ -3,6 +3,7 @@ package com.julioprojects.appplantinhas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
     }
 }
