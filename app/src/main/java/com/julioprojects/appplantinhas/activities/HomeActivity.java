cb904d2cdb6f51c6dee4e5904471de19cf918f5d
Lucas Calderon <lucas8calderon@hotmail.com>
2022-09-13 16:10:25 -0300

    recyclerview

--- app/src/main/java/com/julioprojects/appplantinhas/activities/HomeActivity.java
+++ app/src/main/java/com/julioprojects/appplantinhas/activities/HomeActivity.java
@@ -1,16 +1,36 @@
 package com.julioprojects.appplantinhas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Adapter;
 
 import com.julioprojects.appplantinhas.R;
 
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewPlants;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        recyclerViewPlants = findViewById(R.id.recyclerPlants);
+
+
+
+
+        recyclerViewPlants.setHasFixedSize(true);
+        recyclerViewPlants.setLayoutManager(new LinearLayoutManager(this));
+       // recyclerViewPlants.setAdapter();
+
     }
 }
