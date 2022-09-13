7a12ce676ae1a250a221bea6beaf51f67ff899ff
jrodrigues <julio.rodrigues@foursys.com.br>
2022-09-13 13:15:05 -0300

    Navegação para register

--- app/src/main/java/com/julioprojects/appplantinhas/activities/LoginActivity.java
+++ app/src/main/java/com/julioprojects/appplantinhas/activities/LoginActivity.java
@@ -2,19 +2,39 @@ package com.julioprojects.appplantinhas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.julioprojects.appplantinhas.R;
+import com.julioprojects.appplantinhas.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        // Ações de clique
+        binding.tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegister();
+            }
+        });
 
 
 
+    }
+
+    private void openRegister() {
+        startActivity(new Intent(LoginActivity.this, Register.class));
     }
 }
